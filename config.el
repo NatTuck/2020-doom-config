4a3747095b12c278cbc938854f93cd8f1a1c5087
Nat Tuck <nat@ferrus.net>
2019-05-21 16:09:32 -0400

    Fix :q and :wq

--- config.el
+++ config.el
@@ -13,6 +13,21 @@
             (auto-insert-mode 0)
             ))
 
+(defun liu233w/ex-kill-buffer-and-close ()
+  (interactive)
+  (unless (char-equal (elt (buffer-name) 0) ?*)
+    (kill-this-buffer))
+  )
+
+(defun liu233w/ex-save-kill-buffer-and-close ()
+  (interactive)
+  (save-buffer)
+  (kill-this-buffer)
+  )
+
+(evil-ex-define-cmd "q[uit]" 'liu233w/ex-kill-buffer-and-close)
+(evil-ex-define-cmd "wq" 'liu233w/ex-save-kill-buffer-and-close)
+
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
   (setq web-mode-enable-auto-pairing nil)
@@ -23,10 +38,12 @@
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
-)
+  )
 (add-hook 'web-mode-hook  'my-web-mode-hook)
 (add-to-list 'auto-mode-alist '("\\.eex$" . web-mode))
 
 (defun my-js2-mode-hook ()
   (setq js2-basic-offset 2))
 (add-hook 'js2-mode-hook 'my-js2-mode-hook)
+
+(define-key evil-normal-state-map "Q" 'fill-paragraph)
